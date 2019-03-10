78a416da211adc8063f7516c38e0ac64e9fa1b1f
Magnus Hartvig Groenbech <groenbech96@gmail.com>
2019-03-10 11:21:17 +0100

    shell files

---
+++ shell/setup.sh
@@ -0,0 +1,16 @@
+# Setup virtual env
+export PYTHONPATH=
+python3 -m venv mlpy3env
+source mlpy3env/bin/activate
+
+# Upgrade pip
+pip install -U pip
+
+# install 
+pip install -r requirements.txt
+
+# stop virtual env
+deactivate
+chmod -R 777 mlpy3env
+
+
