c46ed88c227f804d8f60df77d57c415aa0b9b7c6
Magnus Hartvig Groenbech <groenbech96@gmail.com>
2019-04-27 20:22:00 +0200

    added converter

---
+++ data/converter.py
@@ -0,0 +1,32 @@
+import rawpy
+import numpy as np
+import glob
+import imageio
+
+
+
+x1_files = glob.glob('train/long/*.ARW')
+x2_files = glob.glob('train/short/*.ARW')
+x3_files = glob.glob('test/long/*.ARW')
+x4_files = glob.glob('train/short/*.ARW')
+
+for f in x1_files:
+    with rawpy.imread(f) as raw:
+        rgb = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+        imageio.imsave(f[:-4]+'.jpg', rgb)
+
+for f in x2_files:
+    with rawpy.imread(f) as raw:
+        rgb = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+        imageio.imsave(f[:-4]+'.jpg', rgb)
+
+for f in x3_files:
+    with rawpy.imread(f) as raw:
+        rgb = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+        imageio.imsave(f[:-4]+'.jpg', rgb)
+
+for f in x4_files:
+    with rawpy.imread(f) as raw:
+        rgb = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+        imageio.imsave(f[:-4]+'.jpg', rgb)
+
