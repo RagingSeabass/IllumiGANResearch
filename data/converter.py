7615e95338d7cc44bfa0258bfd8a343454e35971
Magnus Hartvig Groenbech <groenbech96@gmail.com>
2019-04-28 12:23:34 +0200

    Fixed an issue with spelling mistake

--- data/converter.py
+++ data/converter.py
@@ -8,22 +8,22 @@ import imageio
 x1_files = glob.glob('data/train/long/*.ARW')
 x2_files = glob.glob('data/train/short/*.ARW')
 x3_files = glob.glob('data/test/long/*.ARW')
-x4_files = glob.glob('data/train/short/*.ARW')
+x4_files = glob.glob('data/test/short/*.ARW')
 
-for f in x1_files:
-    with rawpy.imread(f) as raw:
-        rgb = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
-        imageio.imsave(f[:-4]+'.jpg', rgb)
+# for f in x1_files:
+#     with rawpy.imread(f) as raw:
+#         rgb = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+#         imageio.imsave(f[:-4]+'.jpg', rgb)
 
-for f in x2_files:
-    with rawpy.imread(f) as raw:
-        rgb = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
-        imageio.imsave(f[:-4]+'.jpg', rgb)
+# for f in x2_files:
+#     with rawpy.imread(f) as raw:
+#         rgb = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+#         imageio.imsave(f[:-4]+'.jpg', rgb)
 
-for f in x3_files:
-    with rawpy.imread(f) as raw:
-        rgb = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
-        imageio.imsave(f[:-4]+'.jpg', rgb)
+# for f in x3_files:
+#     with rawpy.imread(f) as raw:
+#         rgb = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+#         imageio.imsave(f[:-4]+'.jpg', rgb)
 
 for f in x4_files:
     with rawpy.imread(f) as raw:
