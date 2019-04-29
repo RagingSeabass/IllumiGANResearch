dbbb0011f0f6164cf8794978b2dd3a5278ff03d4
Magnus Hartvig Groenbech <groenbech96@gmail.com>
2019-04-29 12:34:05 +0200

    added running on 1 GPU

--- shell/train.sh
+++ shell/train.sh
@@ -8,7 +8,7 @@
 #BSUB -q gpuv100
 
 ### -- set the job Name --
-#BSUB -J LTS_PYT2x
+#BSUB -J LTS_PYTJPG
 
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 2
@@ -23,7 +23,7 @@
 #BSUB -R "select[sxm2]"
 
 ### -- Select the resources: 1 gpu in exclusive proce   ss mode --
-#BSUB -gpu "num=2:mode=exclusive_process"
+#BSUB -gpu "num=1:mode=exclusive_process"
 
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
 #BSUB -W 24:00
@@ -34,7 +34,7 @@
 ### -- set the email address --
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
-##BSUB -u s164433@student.dtu.dk
+##BSUB -u s164444@student.dtu.dk
 
 ### -- send notification at completion--
 #BSUB -N
