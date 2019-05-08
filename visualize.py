9563dad1ade3f5234526671ca706add83274d570
Magnus Hartvig Groenbech <groenbech96@gmail.com>
2019-05-08 15:48:32 +0200

    vixz

--- visualize.py
+++ visualize.py
@@ -1,3 +1,19 @@
 
-# Visualize training 
+# Load visualization into csv
+import re
+
+file = 'output/_default/reports/train.log'
+lines = [line.rstrip('\n') for line in open(file)]
+
+for line in lines:
+
+    # Get date: \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}
+    date = re.match(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', line).group()
+    
+    
+
+
+
+
+
 
