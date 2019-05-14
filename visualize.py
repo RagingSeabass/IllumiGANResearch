1ff63741590c4bbd39f427de18611e6900f147b6
Magnus Hartvig Groenbech <groenbech96@gmail.com>
2019-05-14 11:44:46 +0200

    Added vizulizer

--- visualize.py
+++ visualize.py
@@ -1,17 +1,54 @@
 
 # Load visualization into csv
 import re
+import matplotlib.pyplot as plt
 
-file = 'output/_default/reports/train.log'
-lines = [line.rstrip('\n') for line in open(file)]
 
-for line in lines:
+files = ['output/_default/reports/train.log']
+#lines = [line.rstrip('\n') for line in open(file)]
 
-    # Get date: \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}
-    date = re.match(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', line).group()
-    
-    
 
+epochs = []
+lossGs = []
+lossDs = []
+averageTime = 0
+totalIteartions = 0
+startTime = ""
+times = []
+first = True
+for file in files:
+    iterations = 0
+    lines = [line.rstrip('\n') for line in open(file)]
+    for line in lines:
+         
+        if first:
+            # Get time and date of entry
+            startTime = re.search(r'\d{4}-\d{2}-\d{2}', line).group()
+            # time = re.search(r'\d{2}[":"]\d{2}[":"]\d{2}', line).group()
+            first = False
+
+        epoch = re.search(r'Epoch [0-9]*', line)
+        if (epoch):
+            epoch = epoch.group()[6:] # Remove text
+            epochs.append(float(epoch))
+            lossG = re.search(r'Loss G: [0-9]*["."][0-9]*[e]*-*[0-9]*', line).group()[7:]
+            lossGs.append(float(lossG))
+            lossD = re.search(r'D: [0-9]*["."][0-9]*[e]*-*[0-9]*', line).group()[3:]
+            lossDs.append(float(lossD))
+            epochTime =  re.search(r'Time [0-9]*["."][0-9]{3}', line).group()[5:]
+            times.append(float(epochTime))
+            iterations = re.search(r'Iteration [0-9]*', line).group()[10:]
+
+    totalIteartions += int(iterations)
+
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot(epochs, lossGs)
+
+plt.subplot(212)
+plt.plot(epochs, lossDs)
+plt.show()
 
 
 
