e0b1d40638bed7281c2dc863fa89f8a7f4f22320
Magnus Hartvig Groenbech <groenbech96@gmail.com>
2019-06-02 15:00:23 +0200

    ok

--- data/png_dataset.py
+++ data/png_dataset.py
@@ -131,6 +131,8 @@ class PNGDataset(Dataset):
 
                 self.number_of_pairs += 1
 
+            manager.get_logger("system").info(f"Loaded PNG dataset | {self.number_of_pairs} image pairs")
+
     def get_exposure(self, path):
         """Get exposure from title"""
         _, fn = os.path.split(path)
