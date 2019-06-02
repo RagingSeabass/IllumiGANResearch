6b3478e18deade822fe813788fc5be065d35fd64
Magnus Hartvig Groenbech <groenbech96@gmail.com>
2019-06-03 01:36:48 +0200

    oktetest

--- export.py
+++ export.py
@@ -81,22 +81,22 @@ def convert_multiarray_output_to_image(spec, feature_name, is_bgr=False):
  
 
 # working code
-#manager = TrainManager(base_dir=base_dir,
-#                       options_f_dir=options,
-#                       hyperparams_f_dir=hyperparams)
+manager = TrainManager(base_dir=base_dir,
+                      options_f_dir=options,
+                      hyperparams_f_dir=hyperparams)
 
-#dataset = JPGDataset(manager, 'short', 'long', transforms=True)
-#dataset = PNGDataset(manager, 'in', 'out', transforms=True)
-#dataloader = DataLoader(dataset, batch_size=manager.get_hyperparams().get(
-#    'batch_size'), shuffle=True, num_workers=0)
+dataset = JPGDataset(manager, 'short', 'long', transforms=True)
+dataset = PNGDataset(manager, 'in', 'out', transforms=True)
+dataloader = DataLoader(dataset, batch_size=manager.get_hyperparams().get(
+   'batch_size'), shuffle=True, num_workers=0)
 
-#model = IllumiganModel(manager=manager)
+model = IllumiganModel(manager=manager)
 
-#dummy_input = torch.randn(1, 3, 1024, 1024).to(manager.device)
+dummy_input = torch.randn(1, 3, 1024, 1024).to(manager.device)
 
-#torch.onnx.export(model.generator_net, dummy_input, "Illumigan.onnx")
+torch.onnx.export(model.generator_net, dummy_input, "Illumigan.onnx")
 
-#onnx_model = onnx.load('./Illumigan.onnx')
+onnx_model = onnx.load('./Illumigan.onnx')
 
 # If we normalize between -1 and 1 
 # use this scale 
@@ -119,8 +119,8 @@ args = dict(
     blue_bias = 0,
     image_scale = scale
 )
-#mlmodel = convert(onnx_model, image_input_names='0', preprocessing_args=args, image_output_names=['133']) # This is what makes it an image lol 
-#mlmodel.save('Illumigan.mlmodel')
+mlmodel = convert(onnx_model, image_input_names='0', preprocessing_args=args)#, image_output_names=['133']) # This is what makes it an image lol 
+mlmodel.save('Illumigan.mlmodel')
 
 coreml_model = coremltools.models.MLModel('Illumigan.mlmodel')
 spec = coreml_model.get_spec()
@@ -156,7 +156,7 @@ spec_layers = getattr(spec,spec.WhichOneof("Type")).layers
 last_layer = spec_layers[-1]
 print(last_layer)
 # Mark the new layer as image
-#convert_multiarray_output_to_image(spec, output_description.name, is_bgr=False)
+convert_multiarray_output_to_image(spec, output_description.name, is_bgr=False)
 
 updated_model = coremltools.models.MLModel(spec)
  
