942240526b623d94c64cb4521236f4be40633ecc
Magnus Hartvig Groenbech <groenbech96@gmail.com>
2019-06-04 12:19:54 +0200

    test

--- data/dng_image.py
+++ data/dng_image.py
@@ -31,12 +31,12 @@ class DNG():
         #raw_matix = np.rot90(raw_matix)
 
         print(raw_matix.shape)
-        scale_percent = 1.1 # percent of original size
-        width = int(raw_matix.shape[1] + 208)
-        height = int(raw_matix.shape[0] + 208)
-        dim = (width, height)
+        #scale_percent = 1.1 # percent of original size
+        #width = int(raw_matix.shape[1] + 208)
+        #height = int(raw_matix.shape[0] + 208)
+        #dim = (width, height)
 
-        raw_matix = cv2.resize(raw_matix, dim)
+        #raw_matix = cv2.resize(raw_matix, dim)
         raw_matix = np.expand_dims(raw_matix, axis=2)
         
         #raw_matix = cv2.resize(raw_matix, dsize=(3024, 4240))
