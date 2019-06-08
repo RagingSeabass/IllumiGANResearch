bbd50b34cdae0050fc8a5375dd6c79081a0323bd
Magnus Hartvig Groenbech <groenbech96@gmail.com>
2019-06-09 00:09:28 +0200

    test

--- export.py
+++ export.py
@@ -104,7 +104,7 @@ content_image = content_image.unsqueeze(0).to(manager.device)
 
 testMod = nn.Sequential(model.generator_net, UnNorm())
 
-dummy_input = torch.randn(1, 3, 1440, 1440).to(manager.device)
+dummy_input = torch.randn(1, 3, 1980, 1024).to(manager.device)
 torch.onnx.export(testMod, dummy_input, "Illumigan.onnx")
 
 onnx_model = onnx.load('./Illumigan.onnx')
